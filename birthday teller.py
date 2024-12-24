23b263b846ba55ccfedfe00229571e05362bd431
toxic123MMM <aashish02@gmail.com>
2024-12-24 22:23:20 +0400

    Add files via upload

---
+++ birthday teller.py
@@ -0,0 +1,11 @@
+Manansh_birthday = "2013-05-18"
+Ashish_birthday = "1986-02-5"
+Deepa_birthday = "1986-8-27"
+khianna_birthday = "2019-05-24"
+Mohan_birthday = "1990-12-05"
+
+print("Manansh Birthday :", Manansh_birthday)
+print("Ashish Birthday :", Ashish_birthday)
+print("Deepa Birthday :", Deepa_birthday)
+print("Khianna Birthday :", khianna_birthday)
+print("Mohan Birthday :", Mohan_birthday)
